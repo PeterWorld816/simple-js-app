1a65d373200ee480627eb29a34e90cfb28338437
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-05 22:14:11 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,31 @@
 let pokemonList =[
 
     {name: 'balbasure', type: "grass", height: 0.5},
-    {name:'charmander', type: 'fire', height: 0.6},
-    {name:'squirtle', type: 'water', height: 0.5} 
+    {name:'charmander', type: 'fire', height: 1},
+    {name:'squirtle', type: 'water', height: 0.2} 
 
 ];
 
     //for loop to iterate over each item in pokemonList
-for (let i = 0; i <pokemonList.length; i++)
-{
-   let currentPokemon=pokemonList[i];
+    for (let i = 0; i <pokemonList.length; i++)
+    {
+       let currentPokemon=pokemonList[i];
+    
+       //Using document.write() to write to the DOM
+       document.write(currentPokemon.name+' height:' +currentPokemon.height+'<br>');
+    
+    }
 
-   //Using document.write() to write to the DOM
-   document.write(currentPokemon.name+'height:' +currentPokemon.height);
+for (let i=0; i<pokemonList.length; i++) {
+    if(pokemonList[i].height < 1 && pokemonList[i].height > 0.4){
+        console.log(currentPokemon.name + 'mid-size pokemon');
+    }else if(pokemonList[i]>0.9){
+        console.log(currentPokemon.name+'big Pokemon');
+    }else{
+        console.log(currentPokemon.name + 'small pokemon');
+    }
+        
+    }
 
-}
+
+    
