312078cd7470848703309422922b82ce7993d823
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-05 21:56:34 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-let pokemons={name:'balbasure', type: "grass", height: 0.5}
+//pokemonList array defined
 
-let pokemons2={name:'charmander', type: 'fire', height: 0.6}
+let pokemonList =[
 
-let pokemons3={name:'squirtle', type: 'water', height: 0.5} 
+    {name: 'balbasure', type: "grass", height: 0.5},
+    {name:'charmander', type: 'fire', height: 0.6},
+    {name:'squirtle', type: 'water', height: 0.5} 
 
+];
+
+    //for loop to iterate over each item in pokemonList
+for (let i = 0; i <pokemonList.length; i++)
+{
+   let currentPokemon=pokemonList[i];
+
+   //Using document.write() to write to the DOM
+   document.write(currentPokemon.name+'height:' +currentPokemon.height);
+
+}
