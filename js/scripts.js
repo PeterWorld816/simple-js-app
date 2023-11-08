7f241f3cd7b01aea6c81e123953604009b2f15a1
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-07 22:31:48 -0500

    adding new pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,20 @@
 //pokemonList array defined
-
 let pokemonList = [
-    { name: 'balbasure', type: ['grass'], height: 0.5 },
-    { name: 'charmander', type: ['fire', 'grass'], height: 1 },
-    { name: 'squirtle', type: ['water'], height: 0.2 }
+    { name: 'Ivysaur', type: ['grass'], height: 0.5 },
+    { name: 'Charmeleon', type: ['fire', 'grass'], height: 1 },
+    { name: 'Watortle', type: ['water'], height: 0.2 }
 ];
 
-//for loop to iterate over each item in pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-    let currentPokemon = pokemonList[i];
+//for loop to iterate over each object within the array
 
-    //Using document.write() to write to the DOM
-    document.write(currentPokemon.name + ' height:' + currentPokemon.height + '<br>');
-
-}
 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height < 1 && pokemonList[i].height > 0.4) {
-        console.log(pokemonList[i].name + ' is mid-size pokemon');
+        console.log(pokemonList[i].name + '');
     } else if (pokemonList[i].height < 0.3) {
-        console.log(pokemonList[i].name + ' is small Pokemon');
+        console.log(pokemonList[i].name + '');
     } else {
         console.log(pokemonList[i].name + ' is large pokemon');
     }
 
 }
-
-
-
