8c51dcbd6b29801d51189290108012eeaa83ffa7
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-07 22:04:31 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
 //pokemonList array defined
 
-let pokemonList =[
-    {name: 'balbasure', type: "grass", height: 0.5},
-    {name:'charmander', type: 'fire', height: 1},
-    {name:'squirtle', type: 'water', height: 0.2} 
+let pokemonList = [
+    { name: 'balbasure', type: ['grass'], height: 0.5 },
+    { name: 'charmander', type: ['fire', 'grass'], height: 1 },
+    { name: 'squirtle', type: ['water'], height: 0.2 }
 ];
 
-   //for loop to iterate over each item in pokemonList
-   for (let i = 0; i <pokemonList.length; i++)
-   {
-      let currentPokemon=pokemonList[i];
-   
-      //Using document.write() to write to the DOM
-      document.write(currentPokemon.name+' height:' +currentPokemon.height+'<br>');
-   
-   }
+//for loop to iterate over each item in pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+    let currentPokemon = pokemonList[i];
 
-for (let i=0; i<pokemonList.length; i++) {
-    if(pokemonList[i].height < 1 && pokemonList[i].height > 0.4){
+    //Using document.write() to write to the DOM
+    document.write(currentPokemon.name + ' height:' + currentPokemon.height + '<br>');
+
+}
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.4) {
         console.log(pokemonList[i].name + ' is mid-size pokemon');
-    }else if(pokemonList[i].height<0.3){
-        console.log(pokemonList[i].name+' is small Pokemon');
-    }else{
+    } else if (pokemonList[i].height < 0.3) {
+        console.log(pokemonList[i].name + ' is small Pokemon');
+    } else {
         console.log(pokemonList[i].name + ' is large pokemon');
-    }    
-
     }
 
+}
 
-    
+
+
