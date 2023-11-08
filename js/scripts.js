3c1c5e00d36e36649ce3a9b1586133f977dc7b08
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-07 23:18:17 -0500

    one For and one Condition

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ let pokemonList = [
     { name: 'Blastoise', type: ['water'], height: 0.2 }
 ];
 
+//for loop to iterate over each object within the array
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.4) {
+        console.log(pokemonList[i].name + '');
+    } else if (pokemonList[i].height < 0.3) {
+        console.log(pokemonList[i].name + '');
+    } else {
+        console.log(pokemonList[i].name + ' is large pokemon');
+    }
+
+}
