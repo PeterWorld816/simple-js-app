4e92f131d92b7021cc0fb2ead3b26bf9231bd2e8
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-11-07 22:17:50 -0500

    Fix array string and IDE

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,11 @@ let pokemonList = [
     { name: 'squirtle', type: ['water'], height: 0.2 }
 ];
 
-//for loop to iterate over each item in pokemonList
-for (let i = 0; i < pokemonList.length; i++) {
-    let currentPokemon = pokemonList[i];
-
-    //Using document.write() to write to the DOM
-    document.write(currentPokemon.name + ' height:' + currentPokemon.height + '<br>');
-
-}
-
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height < 1 && pokemonList[i].height > 0.4) {
-        console.log(pokemonList[i].name + ' is mid-size pokemon');
+        console.log(pokemonList[i].name + '');
     } else if (pokemonList[i].height < 0.3) {
-        console.log(pokemonList[i].name + ' is small Pokemon');
+        console.log(pokemonList[i].name + '');
     } else {
         console.log(pokemonList[i].name + ' is large pokemon');
     }
