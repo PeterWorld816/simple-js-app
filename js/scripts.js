ff956ffbd6e5b16274cc931167085168cf3724fa
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2023-12-20 21:06:29 -0500

    1.5 update remove line 1 to 21

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,49 @@
-
-//pokemonList array defined
-/*
-let pokemonList = [
-    { name: 'Venusaur', type: ['grass'], height: 0.5 },
-    { name: 'Charizard', type: ['fire', 'grass'], height: 1 },
-    { name: 'Blastoise', type: ['water'], height: 0.2 }
-];
-*/
-//for loop to iterate over each object within the array
-
-/*
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 0.9) {
-        document.write(pokemonList[i].name+ '(height:' + pokemonList[i].height+ ') Wow, that’s big! <br>');
-   } else{
-        document.write(pokemonList[i].name +  '(height:' + pokemonList[i].height +') <br>');
-   }
-}
-
-*/
-
-
-
-
 //1.5 Part 2
 
 var pokemonRepository = (function () {
-    // Private array to store Pokemon
-    var pokemonList = [
-      { name: 'Venusaur', type: ['grass'], height: 0.5 },
-      { name: 'Charizard', type: ['fire', 'grass'], height: 1 },
-      { name: 'Blastoise', type: ['water'], height: 0.2 }
-    ];
-  
-    var publickAPI = {
-      getAll: getAll,
-      add: add
-    };
+  // Private array to store Pokemon
+  var pokemonList = [
+    { name: 'Venusaur', type: ['grass'], height: 0.5 },
+    { name: 'Charizard', type: ['fire', 'grass'], height: 1 },
+    { name: 'Blastoise', type: ['water'], height: 0.2 }
+  ];
 
-    //Return the public object
-    return publickAPI;
+  var publickAPI = {
+    getAll: getAll,
+    add: add
+  };
 
-    //Public function to get all Pokemon
-    function getAll(){
-      return pokemonList;
-    }
+  //Return the public object
+  return publickAPI;
 
-    //Public function to add a single Pokemon
-    function add(pokemon){
-      pokemonList.push(pokemon);
-    }
+  //Public function to get all Pokemon
+  function getAll() {
+    return pokemonList;
+  }
+
+  //Public function to add a single Pokemon
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 })();
 
-  
-  // Example usage:
-  var newPokemon = { name: 'Squirtle', type: ['water'], height: 0.3 };
-  pokemonRepository.add(newPokemon);
-  
-  var newPokemon = { name: 'Squirtle2', type: ['water'], height: 0.3 };
-  pokemonRepository.add(newPokemon);
 
-  // Example usage to retrieve the pokemonList
-  var allPokemon = pokemonRepository.getAll();
-  console.log(allPokemon);
+// Example usage:
+var newPokemon = { name: 'Squirtle', type: ['water'], height: 0.3 };
+pokemonRepository.add(newPokemon);
 
-  //1.5 Part1 
+// Example usage to retrieve the pokemonList
+var allPokemon = pokemonRepository.getAll();
+console.log(allPokemon);
+
+//1.5 Part1 
 
 
-allPokemon.forEach(function(pokemon)
-{
-    if(pokemon.height>0.9){
-        document.write(pokemon.name+'(height:' + pokemon.height +')Wow, that"s big! <br>');
-    }
-    else{
-        document.write(pokemon.name+'(height:' + pokemon.height+')<br>');
-    }
+allPokemon.forEach(function (pokemon) {
+  if (pokemon.height > 0.9) {
+    document.write(pokemon.name + '(height:' + pokemon.height + ')Wow, that"s big! <br>');
+  }
+  else {
+    document.write(pokemon.name + '(height:' + pokemon.height + ')<br>');
+  }
 });
