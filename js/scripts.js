f1167cfdbea440c3f8195370f0a51e58724a672f
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2024-02-26 23:56:12 -0500

    1.10 contrast change

--- js/scripts.js
+++ js/scripts.js
@@ -20,22 +20,22 @@ let pokemonRepository = (function () {
 
     function addListItem(pokemon) {
         let pokemonList = document.querySelector('.pokemon-list');
-    
+
         // Create a new li element
         let li = document.createElement('li');
         li.classList.add('list-group-item'); // Add 'list-group-item' class
-    
+
         // Create a new button element
         let button = document.createElement('button');
-        button.classList.add('btn', 'btn-primary');
+        button.classList.add('btn', 'btn-warning');
         button.textContent = pokemon.name; // Assuming each Pokemon has a 'name' property
-    
+
         // Append the button to the li element
         li.appendChild(button);
-    
+
         // Append the li element to the Pokemon list
         pokemonList.appendChild(li);
-    
+
         // Add an event listener to the button
         button.addEventListener("click", function () {
             showDetails(pokemon);
@@ -82,37 +82,37 @@ let pokemonRepository = (function () {
         let modalBody = document.querySelector('.modal-body');
         let modalHeader = document.querySelector('.modal-header');
         modalBody.innerHTML = '';
-    
+
         let modalTitle = document.querySelector('.modal-title');
         var capitalizedName = pokemon.name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
         modalTitle.innerHTML = capitalizedName;
-    
+
         let closeButtonElement = document.querySelector('.close');
-    
+
         let imageFront = document.createElement('img');
         imageFront.classList.add('modal-img');
         imageFront.src = pokemon.imageUrl;
         imageFront.alt = 'Front image of ' + pokemon.name;
 
-    
+
         let typesElement = document.createElement('p');
         let types = [pokemon.types[0].type.name];
         for (let i = 1; i < pokemon.types.length; i++) {
-          types.push(', ' + pokemon.types[i].type.name);
+            types.push(', ' + pokemon.types[i].type.name);
         }
         typesElement.innerHTML = 'Types: ' + types.join('');
-    
+
         let heightElement = document.createElement('p');
         heightElement.innerHTML = 'Height: ' + pokemon.height;
-    
-        
-    
+
+
+
         modalHeader.appendChild(modalTitle);
         modalHeader.appendChild(closeButtonElement);
         modalBody.appendChild(imageFront);
         modalBody.appendChild(typesElement);
         modalBody.appendChild(heightElement);
-      }
+    }
 
 
     function showDetails(pokemon) {
